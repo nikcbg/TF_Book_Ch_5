a5dae46d6873179f6af877b5f7e98bb9c336a6ce
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-02-26 11:29:39 +0200

    Create main.tf

---
+++ loops-and-if-statements/live/global/existing-iam-user/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "existing_user" {
+  # You should change this to the username of an IAM user that already
+  # exists so you can practice using the terraform import command
+  name = "your_AWS_user_name_here"
+}
