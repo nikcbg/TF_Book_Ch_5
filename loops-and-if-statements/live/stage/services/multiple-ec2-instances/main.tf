915d0b83e51b9ea254c88bbe9267b5e8cbaf129a
nikolay <nikcbg@yahoo.com>
2019-07-03 11:13:07 +0300

    update tf code to 0.12

--- loops-and-if-statements/live/stage/services/multiple-ec2-instances/main.tf
+++ loops-and-if-statements/live/stage/services/multiple-ec2-instances/main.tf
@@ -2,12 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_availability_zones" "all" {}
+data "aws_availability_zones" "all" {
+}
 
 resource "aws_instance" "example" {
-  count             = "${var.num_availability_zones}"
-  availability_zone = "${element(data.aws_availability_zones.all.names, count.index)}"
+  count             = var.num_availability_zones
+  availability_zone = element(data.aws_availability_zones.all.names, count.index)
 
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
 }
+
--- loops-and-if-statements/live/stage/services/multiple-ec2-instances/variables.tf
+++ loops-and-if-statements/live/stage/services/multiple-ec2-instances/variables.tf
@@ -2,3 +2,4 @@ variable "num_availability_zones" {
   description = "The number of Availability Zones in the AWS region"
   default     = 3
 }
+
