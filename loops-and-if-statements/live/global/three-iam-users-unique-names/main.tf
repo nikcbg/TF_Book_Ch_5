7156fbfef7d7a80a35567fc1cb92af4291e0780b
nikolay <nikcbg@yahoo.com>
2019-07-03 11:06:56 +0300

    update tf code to 0.12

--- loops-and-if-statements/live/global/three-iam-users-unique-names/main.tf
+++ loops-and-if-statements/live/global/three-iam-users-unique-names/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 resource "aws_iam_user" "example" {
-  count = "${length(var.user_names)}"
-  name  = "${element(var.user_names, count.index)}"
+  count = length(var.user_names)
+  name  = element(var.user_names, count.index)
 }
 
 resource "aws_iam_policy" "ec2_read_only" {
   name   = "ec2-read-only"
-  policy = "${data.aws_iam_policy_document.ec2_read_only.json}"
+  policy = data.aws_iam_policy_document.ec2_read_only.json
 }
 
 data "aws_iam_policy_document" "ec2_read_only" {
@@ -21,14 +21,14 @@ data "aws_iam_policy_document" "ec2_read_only" {
 }
 
 resource "aws_iam_user_policy_attachment" "ec2_access" {
-  count      = "${length(var.user_names)}"
-  user       = "${element(aws_iam_user.example.*.name, count.index)}"
-  policy_arn = "${aws_iam_policy.ec2_read_only.arn}"
+  count      = length(var.user_names)
+  user       = element(aws_iam_user.example.*.name, count.index)
+  policy_arn = aws_iam_policy.ec2_read_only.arn
 }
 
 resource "aws_iam_policy" "cloudwatch_read_only" {
   name   = "cloudwatch-read-only"
-  policy = "${data.aws_iam_policy_document.cloudwatch_read_only.json}"
+  policy = data.aws_iam_policy_document.cloudwatch_read_only.json
 }
 
 data "aws_iam_policy_document" "cloudwatch_read_only" {
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "cloudwatch_read_only" {
 
 resource "aws_iam_policy" "cloudwatch_full_access" {
   name   = "cloudwatch-full-access"
-  policy = "${data.aws_iam_policy_document.cloudwatch_full_access.json}"
+  policy = data.aws_iam_policy_document.cloudwatch_full_access.json
 }
 
 data "aws_iam_policy_document" "cloudwatch_full_access" {
@@ -53,14 +53,16 @@ data "aws_iam_policy_document" "cloudwatch_full_access" {
 }
 
 resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
-  count = "${var.give_neo_cloudwatch_full_access}"
+  count = var.give_neo_cloudwatch_full_access
 
-  user       = "${aws_iam_user.example.0.name}"
-  policy_arn = "${aws_iam_policy.cloudwatch_full_access.arn}"
+  user       = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_full_access.arn
 }
 
 resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
-  count = "${1 - var.give_neo_cloudwatch_full_access}"
+  count = 1 - var.give_neo_cloudwatch_full_access
 
-  user       = "${aws_iam_user.example.0.name}"
-  policy_arn = "${aws_iam_policy.cloudwatch_read_only.arn}"
+  user       = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_read_only.arn
+}
+
--- loops-and-if-statements/live/global/three-iam-users-unique-names/output.tf
+++ loops-and-if-statements/live/global/three-iam-users-unique-names/output.tf
@@ -1,7 +1,8 @@
 output "neo_arn" {
-  value = "${aws_iam_user.example.0.arn}"
+  value = aws_iam_user.example[0].arn
 }
 
 output "all_arns" {
-  value = ["${aws_iam_user.example.*.arn}"]
+  value = [aws_iam_user.example.*.arn]
 }
+
